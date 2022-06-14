4777f7a60bfce48d9abaaa6d0c8a8a63008877c6
Troy <troy.metivier@me.com>
2022-06-14 10:10:43 -0400

    add repeatString Function

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,12 @@
-const repeatString = function() {
+
+const repeatString = function(userWord, num) {
+    let string = "";
+    if (num < 0) return "ERROR"
+    for (let i = 0; i<num; i++){
+        string += userWord;
+        }
+        return string;
+
 
 };
 
