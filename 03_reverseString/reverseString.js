f3c3bcf8445a21bb3f6109f950f9b75c07798c5a
Troy <troy.metivier@me.com>
2022-06-14 10:30:59 -0400

    added reverseString Function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,17 @@
-const reverseString = function() {
+const reverseString = function(str) {
+/*     var splitString = str.split("");
+    var reverseArray = splitString.reverse();
+    var joinArray = reverseArray.join("");
+    return joinArray; */
 
-};
+    // spit method (splits str into an array of strings)
+    // reverse method( reverse string element 0 beomes last ect.)
+    // join method ( joins array to a string)
+    return str.split("").reverse().join("");
+
+    
+}
+
 
 // Do not edit below this line
 module.exports = reverseString;
